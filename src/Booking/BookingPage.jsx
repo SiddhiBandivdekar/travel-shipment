de244934eb91f30cb0ed4a9da0f67e87544f487a
Siddhi <bandivdekarsiddhi@gmail.com>
2023-04-20 01:00:03 +0530

    Basic app setup

---
+++ src/Booking/BookingPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./BookingPage.css";
+
+const BookingPage = () => {
+  return <div>BookingPage</div>;
+};
+
+export default BookingPage;
---
+++ src/Shipping/ShippingDetails.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ShippingDetails.css";
+
+const ShippingDetails = () => {
+  return <div>ShippingDetails</div>;
+};
+
+export default ShippingDetails;
